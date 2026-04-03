c5f313c8ba4ef93449bccd1fb2adda227b81507b
Muhamad Yusup <yusup.muhamad@team.cranium.id>
2026-04-03 15:24:40 +0700

    improve palindrome

--- src/main/java/com/leetcode/string/ValidPalindrome.java
+++ src/main/java/com/leetcode/string/ValidPalindrome.java
@@ -4,24 +4,39 @@ public class ValidPalindrome {
 
     static boolean isPalindrome(String s) {
 
-        int left = 0;
         String letter = s.replaceAll("[^a-zA-Z0-9]","") .toLowerCase();
+        int left = 0;
         int right = letter.length() - 1;
         int mid = (left + right) / 2;
 
         char[] arr = letter.toCharArray();
 
-        for (int j = right; j > mid ; j--) {
-            if (left > mid) {
+        for (; left < mid ; left++) {
+            if (right < mid) {
                 return true;
             }
 
-            if(arr[left] != arr[j]) {
+            if(arr[left] != arr[right]) {
                 return false;
             }
-            left++;
+            right--;
         }
 
+        return true;
+    }
+
+    static boolean isPalindromeWithLoopWhile(String s) {
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            right--;
+            left++;
+        }
         return true;
     }
 
@@ -29,5 +44,6 @@ public class ValidPalindrome {
         String s = "A man, a plan, a canal: Panama";
 
         System.out.println(isPalindrome(s));
+        System.out.println(isPalindromeWithLoopWhile(s));
     }
 }
